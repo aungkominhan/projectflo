41803f49f396dbaa77051196566e78ec8221ef47
unknown <sayakey1210@example.com>
2023-02-01 10:52:09 +0630

    this is googleclone

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("this is console 1")
+const usersDiv = document.querySelector(".usersDiv");
+const userIdAndName = [
+    { id:"1", name: "mg mg"},
+    { id:"2", name: "Aung Aung"},
+    { id:"3", name: "Ko Ko "},
+    { id:"4", name: "Aye Aye"},
+    { id:"5", name: "Mya Mya"},
+    { id:"6", name: "U Ba"},
+    { id:"7", name: "Daw Mya"},
+    { id:"6", name: "U Hla"},
+    { id:"6", name: "U Mya"},
+    { id:"6", name: "U Ko"},
+    { id:"6", name: "U Aung"},
+];
+for(let i = 0; i < userIdAndName.length; i++ ){
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("userDiv");
+    userDiv.textContent = userIdAndName[i].name
+    const img = document.createElement("img");
+    img.src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnvAuOKji-nHQuDLl_SrG_XomAwUdVEgMKBsdWZamP&s"
+    img.classList.add("imgTag")
+    
+    usersDiv.append(userDiv);
+    userDiv.append(img);
+}
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const fs = require("fs");
 const http = require("http");
 const server = http.createServer((req, res)=>{
-  fs.readFile("index.html", (error, data)=>{
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.write(data);
+    if(req.url === "/data"){
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.write(JSON.stringify({name: "mg mg"}));
+        return res.end();
+    }
+    else if(req.url === "/"){
+        fs.readFile("index.html", (error, data)=>{
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.write(data);
+            return res.end();
+          })
+    }
+    else if (req.url === "/script.js"){
+        fs.readFile("script.js", (error, data)=>{
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.write(data);
+            return res.end();
+        })
+    }
+    else{  res.writeHead(404, {'Comtent-type': 'text/plain'});
+    res.write(error);
     return res.end();
-  })
-});
+}
+})     
+  
+
 server.listen(80,()=>{
     console.log("listen port 80")
 })
